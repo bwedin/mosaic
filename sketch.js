e794f9eb43f403a8b7c976d52bb8a0e0e1ee9d93
bwedin <bwedin@broadinstitute.org>
2017-10-09 11:43:34 -0400

    more draggable beziers in bottom

--- sketch.js
+++ sketch.js
@@ -8,13 +8,22 @@ var INIT_COLORS = 3;
 var INIT_COLORSETS = 2;
 var MAX_COLORS = 8;
 var MAX_COLORSETS = 5;
+var NUM_EQ_NODES = 6;
 var activeObject;
+
+var COLORSET_COLOR_1 = "#048ad1";
+var COLORSET_COLOR_2 = "#812050";
+var COLORSET_COLOR_3 = "#8ddc1c";
+var COLORSET_COLOR_4 = "#fd8f2f";
+var COLORSET_COLOR_5 = "#f0e746";
+var COLORSET_ARRAY = [COLORSET_COLOR_1, COLORSET_COLOR_2, COLORSET_COLOR_3, COLORSET_COLOR_4, COLORSET_COLOR_5];
 
 var circleTest;
 var leftBezier;
 var rightBezier;
 
 var shapesToDraw = [];
+var movingCircleArray = [];
 var clickableObjects = [];
 
 $(document).ready(function(){
@@ -35,28 +44,11 @@ $(document).ready(function(){
     //         console.log(tuple);
     //     });
     // });
-
-    // hide all ones we don't want
-    for(i=1; i<=MAX_COLORSETS; i++) {
-        if(i>INIT_COLORSETS) {
-            let colorsetDiv = '#colorset-' + i;
-            $(colorsetDiv).toggle();
-        }
-        for (j=1; j<=MAX_COLORS; j++) {
-            let colorpickerDiv = '#colorset-' + i + '-color-' + j;
-            if (j>INIT_COLORS) {
-                $(colorpickerDiv).toggle();
-            }
-            else {
-                $(function() {
-                    $(colorpickerDiv).colorpicker({
-                        format: 'rgb',
-                        color: '#FFFFFF'
-                    });
-                });
-            }
-        }
-    }
+    document.getElementById('colorset-1-dot').style.color = COLORSET_COLOR_1;
+    document.getElementById('colorset-2-dot').style.color = COLORSET_COLOR_2;
+    document.getElementById('colorset-3-dot').style.color = COLORSET_COLOR_3;
+    document.getElementById('colorset-4-dot').style.color = COLORSET_COLOR_4;
+    document.getElementById('colorset-5-dot').style.color = COLORSET_COLOR_5;
 
 });
 
@@ -120,22 +112,83 @@ function addColorset() {
 // setup p5
 function setup() {
     var canvas = createCanvas(500, 500);
+    windowResized();
     canvas.parent('sketch-holder');
-    background(tuple);
-    background(tuple);
-    c = color('#00ff00');
-    drawTriangle(30,300,300,1,c,0.6);
-    c = color('#0000ff');
-    drawTriangle(30,400,350,-1,c,0.6);
-    c = color('#ff0000');
-    leftBezier = movingBezierHorizontal(100,400,200,200,c);
-    shapesToDraw.push(leftBezier);
-    rightBezier = movingBezierHorizontal(200,200,400,400,c);
-    shapesToDraw.push(rightBezier);
-    circleTest =  movingCircleMidpoint(200,200,10,c,0.1,leftBezier,rightBezier);
-    shapesToDraw.push(circleTest);
-    clickableObjects.push(circleTest);
-    
+    // need a string of bezier curves
+
+    // hide all ones we don't want
+    for(i=1; i<=MAX_COLORSETS; i++) {
+        //adding colorpickers
+        if(i>INIT_COLORSETS) {
+            let colorsetDiv = '#colorset-' + i;
+            $(colorsetDiv).toggle();
+        }
+        for (j=1; j<=MAX_COLORS; j++) {
+            let colorpickerDiv = '#colorset-' + i + '-color-' + j;
+            if (j>INIT_COLORS) {
+                $(colorpickerDiv).toggle();
+            }
+            else {
+                $(function() {
+                    $(colorpickerDiv).colorpicker({
+                        format: 'rgb',
+                        color: '#FFFFFF'
+                    });
+                });
+            }
+        }
+
+        //adding bezier curves/EQ circles to canvas
+        // draw all beziers, add to array
+        // bezier array rhymes, maybe?
+        // add start, iterate through length of beziers-1, add end
+        let bezierArray = [];
+        c = color(COLORSET_ARRAY[i-1]);
+        for(let j=0; j<NUM_EQ_NODES-1; j++) {
+            console.log(j);
+            bezierObj = MovingBezierHorizontal(0,0,canvas.width,0,c);
+            shapesToDraw.push(bezierObj);
+            bezierArray.push(bezierObj);
+            if (i>INIT_COLORSETS) { bezierObj.hide(); }
+        }
+        circleObj =  MovingCircleStartpoint(0,canvas.height-10,10,c,0.1,bezierArray[0]);
+        shapesToDraw.push(circleObj);
+        clickableObjects.push(circleObj);
+        movingCircleArray.push(circleObj);
+        if (i>INIT_COLORSETS) { circleObj.hide(); }
+        for(let j=0; j<bezierArray.length-1; j++) {
+            circleObj =  MovingCircleMidpoint(canvas.width*((j+1)/5),canvas.height-10,10,c,0.1,bezierArray[j],bezierArray[j+1]);
+            shapesToDraw.push(circleObj);
+            clickableObjects.push(circleObj);
+            movingCircleArray.push(circleObj);
+            if (i>INIT_COLORSETS) { circleObj.hide(); }
+        }
+        circleObj =  MovingCircleEndpoint(canvas.width,canvas.height-10,10,c,0.1,bezierArray[bezierArray.length-1]);
+        shapesToDraw.push(circleObj);
+        clickableObjects.push(circleObj);
+        movingCircleArray.push(circleObj);
+        if (i>INIT_COLORSETS) { circleObj.hide(); }
+    }
+}
+
+// width is a scaling transform, height is a vertical offset relative to the bottom
+function repositionMovingCircles(oldWidth, oldHeight) {
+    // windowWidth
+    // windowHeigh
+    let newHeight = canvas.height/2;
+    let newWidth = canvas.width/2;
+    movingCircleArray.forEach( function(obj) {
+        if(obj.lock) {
+            console.log('lock?')
+            obj.unlock();
+            obj.setPosition(obj.getX()*(newWidth/oldWidth),newHeight-(oldHeight-obj.getY()));
+            obj.lock();
+        }
+        else {
+            console.log('confused')
+            obj.setPosition(obj.getX()*(newWidth/oldWidth),newHeight-(oldHeight-obj.getY()));
+        }
+    })
 }
 
 function drawTriangle(sideLength, bottomPosX, bottomPosY, xDirection, color, opacity) {
@@ -162,6 +215,15 @@ const clickableCircle = (obj) => ({
     }
 });
 
+const gettableXY = (obj) => ({
+    getX: () => {
+        return obj.xPos;
+    },
+    getY: () => {
+        return obj.yPos;
+    },
+});
+
 const moveableXY = (obj) => ({
     setPosition: (x,y) => {
         obj.xPos = x;
@@ -179,6 +241,63 @@ const moveableXYMidpointHorizontal = (obj) => ({
         else {
             obj.xPos = x;
         }
+    },
+    rescalePosition: (x,y) => {
+        obj.yPos = y;
+        obj.leftObj.setEnd(window.canvas,y);
+        obj.rightObj.setStart(0,y);
+        obj.leftObj.setEnd(x,y);
+        obj.rightObj.setStart(x,y);
+        obj.xPos = x;
+    }
+});
+
+const moveableXYStartpointHorizontal = (obj) => ({
+    setPosition: (x,y) => {
+        obj.yPos = y;
+        if(!obj.rightObj.setStart(obj.xPos,y)) {
+            obj.leftObj.setEnd(obj.xPos,y)
+        }
+        else if(obj.lock) {
+            obj.leftObj.setEnd(obj.xPos,y)
+        }
+        else {
+            obj.xPos = x;
+        }
+        // don't move x, it should be anchored
+    },
+    hasLock: () => {
+        return true;
+    },
+    unlock: () => {
+        obj.lock = false;
+    },
+    lock: () => {
+        obj.lock = true;
+    }
+});
+
+const moveableXYEndpointHorizontal = (obj) => ({
+    setPosition: (x,y) => {
+        obj.yPos = y;
+        if(!obj.leftObj.setEnd(x,y)) {
+            obj.leftObj.setEnd(obj.xPos,y)
+        }
+        else if(obj.lock) {
+            obj.leftObj.setEnd(obj.xPos,y)
+        }
+        else {
+            obj.xPos = x;
+        }
+    },
+    hasLock: () => {
+        return true;
+    },
+    unlock: () => {
+        obj.lock = false;
+    },
+    lock: () => {
+        obj.lock = true;
     }
 });
 
@@ -189,20 +308,6 @@ const drawableCircle = (obj) => ({
         ellipse(obj.xPos, obj.yPos, obj.radius*2, obj.radius*2);
     }
 });
-
-const movingCircleMidpoint = (xPos,yPos,radius,color,opacity,leftObj,rightObj)  => {
-    var state = {
-        xPos,yPos,radius,color,opacity,leftObj,rightObj
-    }
-    leftObj.setEnd(xPos,yPos);
-    rightObj.setStart(xPos,yPos);
-    return Object.assign(
-        {},
-        clickableCircle(state),
-        drawableCircle(state),
-        moveableXYMidpointHorizontal(state)
-    )
-};
 
 const drawableBezierHorizontal = (obj) => ({
     draw: () => {
@@ -217,6 +322,7 @@ const moveableStartEndXYHorizontal = (obj) => ({
     setStart: (x,y) => {
         obj.yPosStart = y;
         if(obj.xPosEnd<x){
+            console.log('aaa')
             return false;
         }
         else {
@@ -227,6 +333,7 @@ const moveableStartEndXYHorizontal = (obj) => ({
     setEnd: (x,y) => {
         obj.yPosEnd = y;
         if(obj.xPosStart>x){
+            console.log('bbb')
             return false;
         }
         else {
@@ -236,26 +343,94 @@ const moveableStartEndXYHorizontal = (obj) => ({
     }
 });
 
-const movingCircle = (xPos,yPos,radius,color,opacity)  => {
+const hideable = (obj) => ({
+    isShown: () => {
+        return obj.shown;
+    },
+    show: () => {
+        obj.shown = true;
+    },
+    hide: () => {
+        obj.shown = false;
+    },
+});
+
+const MovingCircle = (xPos,yPos,radius,color,opacity)  => {
     var state = {
-        xPos,yPos,radius,color,opacity
+        xPos,yPos,radius,color,opacity,
+        shown: true
     }
     return Object.assign(
         {},
         clickableCircle(state),
         drawableCircle(state),
-        moveableXY(state)
+        moveableXY(state),
+        gettableXY(state),
+        hideable(state)
     )
 };
 
-const movingBezierHorizontal = (xPosStart,yPosStart,xPosEnd,yPosEnd,color)  => {
+const MovingCircleStartpoint = (xPos,yPos,radius,color,opacity,rightObj)  => {
     var state = {
-        xPosStart,yPosStart,xPosEnd,yPosEnd,color
+        xPos,yPos,radius,color,opacity,rightObj,
+        shown: true,
+        lock: true
+    }
+    rightObj.setStart(xPos,yPos);
+    return Object.assign(
+        {},
+        clickableCircle(state),
+        drawableCircle(state),
+        moveableXYStartpointHorizontal(state),
+        gettableXY(state),
+        hideable(state)
+    )
+};
+
+const MovingCircleEndpoint = (xPos,yPos,radius,color,opacity,leftObj)  => {
+    var state = {
+        xPos,yPos,radius,color,opacity,leftObj,
+        shown: true,
+        lock: true
+    }
+    leftObj.setEnd(xPos,yPos);
+    return Object.assign(
+        {},
+        clickableCircle(state),
+        drawableCircle(state),
+        moveableXYEndpointHorizontal(state),
+        gettableXY(state),
+        hideable(state)
+    )
+};
+
+const MovingCircleMidpoint = (xPos,yPos,radius,color,opacity,leftObj,rightObj)  => {
+    var state = {
+        xPos,yPos,radius,color,opacity,leftObj,rightObj,
+        shown: true
+    }
+    leftObj.setEnd(xPos,yPos);
+    rightObj.setStart(xPos,yPos);
+    return Object.assign(
+        {},
+        clickableCircle(state),
+        drawableCircle(state),
+        moveableXYMidpointHorizontal(state),
+        gettableXY(state),
+        hideable(state)
+    )
+};
+
+const MovingBezierHorizontal = (xPosStart,yPosStart,xPosEnd,yPosEnd,color)  => {
+    var state = {
+        xPosStart,yPosStart,xPosEnd,yPosEnd,color,
+        shown: true
     }
     return Object.assign(
         {},
         moveableStartEndXYHorizontal(state),
-        drawableBezierHorizontal(state)
+        drawableBezierHorizontal(state),
+        hideable(state)
     )
 };
 
@@ -293,9 +468,8 @@ const movingBezierHorizontal = (xPosStart,yPosStart,xPosEnd,yPosEnd,color)  => {
 function mousePressed() {
   // Check if mouse is inside the circle
   clickableObjects.forEach( function(obj) {
-    if(obj.isMouseInside()) {
+    if(obj.isMouseInside() && obj.isShown()) {
         activeObject = obj;
-        console.log('found!')
         return;
     }
   })
@@ -307,13 +481,26 @@ function mouseReleased() {
 }
 
 function mouseDragged() {
+    if (mouseY>canvas.height/2-10) {
+        mouseY = canvas.height/2-10;
+    }
+    else if (mouseY<canvas.height/2-140) {
+        mouseY = canvas.height/2-140;
+    }
+    if (mouseX<0) {
+        mouseX = 0;
+    }
+    else if (mouseX>canvas.width/2) {
+        mouseX = canvas.width/2;
+    }
     if(activeObject) {
-        console.log('draggin');
         activeObject.setPosition(mouseX,mouseY);
     }
 }
 
 function windowResized() {
+    oldHeight = canvas.height/2;
+    oldWidth = canvas.width/2;
     if(windowWidth<1350) {
         resizeCanvas(700,150+700*(9/16));
     }
@@ -323,13 +510,27 @@ function windowResized() {
     else {
         resizeCanvas(1200,150+1200*(9/16));
     }
+    if(canvas.width/2>oldWidth) {
+        movingCircleArray.sort(function(a, b) {
+            return b.getX() - a.getX();
+        });
+        repositionMovingCircles(oldWidth,oldHeight);
+    }
+    else if(canvas.width/2<oldWidth) {
+        movingCircleArray.sort(function(a, b) {
+            return a.getX() - b.getX();
+        });
+        repositionMovingCircles(oldWidth,oldHeight);
+    }
 }
 
 // main p5 loop
 function draw() {
     background(200);
     shapesToDraw.forEach( function (shape) {
-        shape.draw();
+        if(shape.isShown()) {
+            shape.draw();
+        }
     })
 	shape = $('#shape-toggle input:radio:checked').val();
 	tuple[1] = tuple[1]+1;
