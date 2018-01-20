fabcbc063aefc5141e3d58b3e1275b8dd2194396
bwedin <bwedin@broadinstitute.org>
2018-01-19 20:24:44 -0500

    autochanging colorset colors, autohide opacity controls

--- sketch.js
+++ sketch.js
@@ -20,13 +20,14 @@ var colorset_color_2 = "#048ad1" ;
 var colorset_color_3 = "#8ddc1c";
 var colorset_color_4 = "#9159de";
 var colorset_color_5 = "#fd8f2f";
-var colorset_array = [colorset_color_1, colorset_color_2, colorset_color_3, colorset_color_4, colorset_color_5];
+var colorsetColorKey = {1: colorset_color_1, 2: colorset_color_2, 3: colorset_color_3,
+  4: colorset_color_4, 5: colorset_color_5};
 var colorsetObjects = {};
 // some default colors
-var fallColorset = ["rgb(227,64,27)", "rgb(235,182,38)", "rgb(246,238,0)", "rgb(76,39,10)",
+var fallColorset = ["rgb(232,19,19)", "rgb(235,182,38)", "rgb(246,238,0)", "rgb(28,9,9)",
   "rgb(227,174,140)", "rgb(151,92,48)", "rgb(156,26,84)", "rgb(228,105,129)"];
-var winterColorset = ["rgb(50,49,238)", "rgb(77,17,148)", "rgb(71,111,216)", "rgb(119,209,253)",
-  "rgb(13,53,63)", "rgb(89,250,234)", "rgb(22,146,148)", "rgb(92,108,131)"];
+var winterColorset = ["rgb(50,49,238)", "rgb(77,17,148)", "rgb(119,209,253)", "rgb(141,87,255)",
+  "rgb(89,250,234)", "rgb(71,111,216)", "rgb(22,146,148)", "rgb(92,108,131)"];
 var springColorset = ["rgb(221,61,202)", "rgb(169,104,210)", "rgb(252,194,251)", "rgb(88,37,105)",
   "rgb(191,214,250)", "rgb(246,86,139)", "rgb(242,145,128)", "rgb(97,8,232)"];
 var summerColorset = ["rgb(11,83,19)", "rgb(42,150,86)", "rgb(87,226,76)", "rgb(195,239,178)",
@@ -36,7 +37,7 @@ var shapesToDraw = [];
 // proportion box variables
 var COLORSET_EQ_BOTTOM = 8;
 var COLORSET_EQ_TOP = 140;
-var CIRCLE_RADIUS = 6;
+var CIRCLE_RADIUS = 8;
 var NUM_EQ_NODES = 6;
 var movingCircleArray = [];
 var clickableObjects = [];
@@ -156,13 +157,17 @@ function setup() {
         for (let j=1; j<=MAX_COLORS; j++) {
             let colorpickerDiv = '#colorset-' + i + '-color-' + j;
             if (j>INIT_COLORS) {
+                $(colorpickerDiv).colorpicker({
+                  format: 'rgb',
+                  color: 'rgb(255,255,254)'
+                });
                 $(colorpickerDiv).toggle();
             }
             else {
                 $(colorpickerDiv).colorpicker({
                         format: 'rgb',
                         color: 'rgb(255,255,254)'
-                  });
+                });
             }
         }
 
@@ -171,28 +176,27 @@ function setup() {
         // add start, iterate through length of beziers-1, add end
         let bezierArray = [];
         let circleArray = [];
-        let c = color(colorset_array[i-1]);
         for(let j=0; j<NUM_EQ_NODES-1; j++) {
-            let bezierObj = MovingBezierHorizontal(0,0,width,0,c);
+            let bezierObj = MovingBezierHorizontal(0,0,width,0,i);
             shapesToDraw.push(bezierObj);
             bezierArray.push(bezierObj);
             if (i>INIT_COLORSETS) { bezierObj.hide(); }
         }
-        let circleObj =  MovingCircleStartpoint(0,height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,c,bezierArray[0],i);
+        let circleObj =  MovingCircleStartpoint(0,height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,i,bezierArray[0],i);
         shapesToDraw.push(circleObj);
         clickableObjects.push(circleObj);
         movingCircleArray.push(circleObj);
         circleArray.push(circleObj);
         if (i>INIT_COLORSETS) { circleObj.hide(); }
         for(let j=0; j<bezierArray.length-1; j++) {
-            circleObj =  MovingCircleMidpoint(width*((j+1)/5),height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,c,bezierArray[j],bezierArray[j+1],i);
+            circleObj =  MovingCircleMidpoint(width*((j+1)/5),height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,i,bezierArray[j],bezierArray[j+1],i);
             shapesToDraw.push(circleObj);
             clickableObjects.push(circleObj);
             movingCircleArray.push(circleObj);
             circleArray.push(circleObj);
             if (i>INIT_COLORSETS) { circleObj.hide(); }
         }
-        circleObj =  MovingCircleEndpoint(width,height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,c,bezierArray[bezierArray.length-1],i);
+        circleObj =  MovingCircleEndpoint(width,height-WINDOW_BOTTOM_PADDING-COLORSET_EQ_BOTTOM,CIRCLE_RADIUS,i,bezierArray[bezierArray.length-1],i);
         shapesToDraw.push(circleObj);
         clickableObjects.push(circleObj);
         movingCircleArray.push(circleObj);
@@ -242,10 +246,15 @@ function findColorRNG(proportions, alphas){
       let alphaChoices = alphaArray[colorsetNumber];
       // todo bw also change this
       // alphaChoices = alphas
-      let alpha = Number(alphaChoices[Math.floor(Math.random()*alphaChoices.length)]);
-      // console.log(alpha);
-      // todo bw change this
-      return [colorRGB.r,colorRGB.g,colorRGB.b,alpha];
+      if(historyFraction>0) {
+        return [colorRGB.r, colorRGB.g, colorRGB.b, 255];
+      }
+      else {
+        let alpha = Number(alphaChoices[Math.floor(Math.random() * alphaChoices.length)]);
+        // console.log(alpha);
+        // todo bw change this
+        return [colorRGB.r, colorRGB.g, colorRGB.b, alpha];
+      }
     }
   }
 }
@@ -298,10 +307,18 @@ function updateColorArray() {
     let colorsetName = 'colorset-' + i;
     let colorCount = colorsetColorCount[colorsetName];
     let colorsetArray = [];
+    let rKey = 0;
+    let gKey = 0;
+    let bKey = 0;
     for(let j=1; j<=colorCount; j++){
       let colorpickerName = '#' + colorsetName + '-color-' + j;
-      colorsetArray.push($(colorpickerName).colorpicker().data('colorpicker').color.toRGB());
+      let newColor = $(colorpickerName).colorpicker().data('colorpicker').color.toRGB();
+      colorsetArray.push(newColor);
+      rKey = rKey + newColor.r;
+      gKey = gKey + newColor.g;
+      bKey = bKey + newColor.b;
     }
+
     colorArray[i] = colorsetArray;
     let alphaName = colorsetName+'-opacity';
     let singleAlphaArray = [];
@@ -313,10 +330,22 @@ function updateColorArray() {
       singleAlphaArray = [255];
     }
     alphaArray[i] = singleAlphaArray;
+    if((rKey+gKey+bKey)/colorCount/3 > 140) {
+      rKey = rKey*0.5;
+      gKey = gKey*0.5;
+      bKey = bKey*0.5
+    }
+
+    rKey = Math.round(rKey/colorCount).toString(16).padStart(2, '0');
+    gKey = Math.round(gKey/colorCount).toString(16).padStart(2, '0');
+    bKey = Math.round(bKey/colorCount).toString(16).padStart(2, '0');
+    let newColorsetColor = '#' + rKey + gKey + bKey;
+    colorsetColorKey[i] = newColorsetColor;
+    $('.' + colorsetName).css('color',newColorsetColor);
   }
 }
-function drawMain() {  
-  shapeType = $('#shape-toggle input:radio:checked').val();
+function drawMain() {
+  let shapeType = $('#shape-toggle input:radio:checked').val();
   if(shapeType!=shape) {
     colorTiles = {};
     shape = shapeType;
@@ -360,30 +389,30 @@ function drawTriangleField(numberColumns,alphaValues) {
   for(let i=0; i<numberColumns; i++) {
     let xPosL = i*columnWidth;
     let yPosTop = 0;
-    drawTriangleColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions[i],alphaValues,numberColumns,i)
+    drawTriangleColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions[i],alphaValues,i)
   }
   // + sidelength*i, orientation=i;
 }
-function drawTriangleColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions,alphaValues,numColumns,columnKey) {
+function drawTriangleColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions,alphaValues,columnKey) {
   let sideLength = 2*columnWidth/Math.sqrt(3);
   let i = 0;
   // draw triangle pointing right, and triangle above it, facing left
   while(yPosTop<=columnHeight) {
     i++;
     let drawColor = findColorRNG(colorsetProportions,alphaValues);
-    drawTriangle(sideLength, xPosL, yPosTop, 1, drawColor, [numColumns,columnKey,i]);
+    drawTriangle(sideLength, xPosL, yPosTop, 1, drawColor, [columnVal,columnKey,i]);
     drawColor = findColorRNG(colorsetProportions,alphaValues);
     i++
-    drawTriangle(sideLength, xPosL+columnWidth, yPosTop-sideLength/2, -1, drawColor, [numColumns,columnKey,i]);
+    drawTriangle(sideLength, xPosL+columnWidth, yPosTop-sideLength/2, -1, drawColor, [columnVal,columnKey,i]);
     yPosTop += sideLength;
   }
   i++;
   // one more for good measure
   let drawColor = findColorRNG(colorsetProportions,alphaValues);
-  drawTriangle(sideLength, xPosL+columnWidth, yPosTop-sideLength/2, -1, drawColor,[numColumns,columnKey,i]);
+  drawTriangle(sideLength, xPosL+columnWidth, yPosTop-sideLength/2, -1, drawColor,[columnVal,columnKey,i]);
 }
 function drawTriangle(sideLength, topPosX, topPosY, xDirection, rngColor, tileKey) {
-  drawColor = getNewColor(rngColor,historyFraction,tileKey);
+  let drawColor = getNewColor(rngColor,historyFraction,tileKey);
   fill(drawColor);
   noStroke();
   // Math.sqrt(3)/2
@@ -410,23 +439,23 @@ function drawSquareField(numberColumns,alphaValues) {
   for(let i=0; i<numberColumns; i++) {
     let xPosL = i*columnWidth;
     let yPosTop = 0;
-    drawSquareColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions[i],alphaValues,numberColumns,i);
+    drawSquareColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions[i],alphaValues,i);
   }
   // + sidelength*i, orientation=i;
 }
-function drawSquareColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions,alphaValues,numColumns,columnKey) {
+function drawSquareColumn(columnWidth,columnHeight,xPosL,yPosTop,colorsetProportions,alphaValues,columnKey) {
   let sideLength = columnWidth; // todo bw: square change here?
   let i = 0;
   // draw squares down to bottom
   while(yPosTop<=columnHeight) {
     i++;
     let drawColor = findColorRNG(colorsetProportions,alphaValues);
-    drawSquare(sideLength, xPosL, yPosTop, drawColor,[numColumns,columnKey,i]);
+    drawSquare(sideLength, xPosL, yPosTop, drawColor,[columnVal,columnKey,i]);
     yPosTop += sideLength;
   }
 }
 function drawSquare(sideLength, leftPosX, topPosY, rngColor, tileKey) {
-  drawColor = getNewColor(rngColor,historyFraction,tileKey);
+  let drawColor = getNewColor(rngColor,historyFraction,tileKey);
   fill(drawColor);
   noStroke();
   rect(leftPosX,topPosY,sideLength,sideLength);
@@ -448,14 +477,14 @@ function drawDiamondField(numberColumns,alphaValues) {
   for(let i=0; i<=numberColumns; i=i+2) {
     let xPosMid = i*columnWidth/2;
     let yPosTop = 0;
-    drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions[i],alphaValues,numberColumns,i);
+    drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions[i],alphaValues,i);
     xPosMid += columnWidth/2;
     yPosTop = yPosTop-(columnWidth/2);
-    drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions[i],alphaValues,numberColumns,i+1);
+    drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions[i],alphaValues,i+1);
   }
   // + sidelength*i, orientation=i;
 }
-function drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions,alphaValues,numColumns,columnKey) {
+function drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProportions,alphaValues,columnKey) {
   let diagonalHalf = columnWidth/2;
   let yPosMid = yPosTop;
   let i = 0;
@@ -463,12 +492,12 @@ function drawDiamondColumn(columnWidth,columnHeight,xPosMid,yPosTop,colorsetProp
   while((yPosMid-diagonalHalf)<=columnHeight) {
     i++;
     let drawColor = findColorRNG(colorsetProportions,alphaValues);
-    drawDiamond(diagonalHalf, xPosMid, yPosMid, drawColor,[numColumns,columnKey,i]);
+    drawDiamond(diagonalHalf, xPosMid, yPosMid, drawColor,[columnVal,columnKey,i]);
     yPosMid += diagonalHalf*2;
   }
 }
 function drawDiamond(diagonalHalf, xPosMid, yPosMid, rngColor, tileKey) {
-  drawColor = getNewColor(rngColor,historyFraction,tileKey);
+  let drawColor = getNewColor(rngColor,historyFraction,tileKey);
   fill(drawColor);
   noStroke();
   quad(xPosMid-diagonalHalf,yPosMid,
@@ -647,14 +676,21 @@ function updateSmoothing() {
   $('#smoothing-display').text(val);
   val++;
   if(val===1) {
+    if(historyFraction>0) {
+      $('.nav-tabs').show(400);
+      $( "#opacity-enabled" ).fadeIn( "slow", function() {
+      });
+    }
     historyFraction = 0;
-    $( ":checkbox" ).removeAttr("disabled");
     $('[name="opacity-lock"]').hide();
   }
   else {
     historyFraction = 1-1/val;
-    $( ":checkbox" ).attr("disabled", true);
-    $('[name="opacity-lock"]').show();
+    $( "#opacity-enabled" ).fadeOut( "fast", function() {
+      // Animation complete
+    });
+    $('[name="nav-link-colors"]').click();
+    $('.nav-tabs').hide(400);
   }
 }
 function updateRefreshRate(inputDiv) {
@@ -702,10 +738,11 @@ function addColor(colorsetDiv) {
     colorsetColorCount[colorsetDiv]++;
     let newDiv = '#' + colorsetDiv + '-color-' + newColorNum;
     $(newDiv).toggle();
-    $(newDiv).colorpicker({
-      format: 'rgb',
-      color: '#FFFFFE'
-    });
+    // todo bw: change back?
+    // $(newDiv).colorpicker({
+    //   format: 'rgb',
+    //   color: '#FFFFFE'
+    // });
   }
   if(colorsetColorCount[colorsetDiv] == MAX_COLORS) {
     $('#add-'+colorsetDiv).prop('disabled', true);
@@ -728,7 +765,6 @@ function removeColorset() {
     $('#remove-colorset').prop('disabled', true);
   }
   let proportions = calculateColorsetProportionsShared(20);
-  updateColorArray();
 }
 function addColorset() {
   $('#remove-colorset').prop('disabled', false);
@@ -903,8 +939,9 @@ const moveableXYEndpointHorizontal = (obj) => ({
 });
 const drawableCircle = (obj) => ({
   draw: () => {
-    fill(obj.color);
-    noStroke();
+    let drawColor = colorsetColorKey[obj.color];
+    fill(color(drawColor));
+    stroke(color(0,0,0));
     ellipse(obj.xPos, obj.yPos, obj.radius*2, obj.radius*2);
     fill(color(255,255,255,255));
     textSize(obj.radius*1.5);
@@ -915,7 +952,7 @@ const drawableBezierHorizontal = (obj) => ({
   draw: () => {
     noFill();
     xLength = obj.xPosEnd-obj.xPosStart;
-    stroke(obj.color);
+    stroke(color(colorsetColorKey[obj.color]));
     bezier(obj.xPosStart, obj.yPosStart, obj.xPosEnd-xLength/4, obj.yPosStart, obj.xPosStart+xLength/4, obj.yPosEnd, obj.xPosEnd, obj.yPosEnd);
   }
 });
